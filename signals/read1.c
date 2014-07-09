28d02ef05e3da537c3f93d88d888667e0bcadaea
ryuever <liuyouchao111@gmail.com>
2014-07-09 23:36:32 +0900

    Add the testing for alarm's return value

--- signals/read1.c
+++ signals/read1.c
@@ -5,17 +5,23 @@ static void	sig_alrm(int);
 int
 main(void)
 {
-	int		n;
-	char	line[MAXLINE];
-
+	int	n;
+    unsigned int alarmRet,alarmRet2;
+	char line[MAXLINE];
+    
 	if (signal(SIGALRM, sig_alrm) == SIG_ERR)
 		err_sys("signal(SIGALRM) error");
 
-	alarm(10);
+	alarmRet = alarm(10);	
+    printf("Testing the time for writing\n");
+
 	if ((n = read(STDIN_FILENO, line, MAXLINE)) < 0)
 		err_sys("read error");
-	alarm(0);
+	alarmRet2 = alarm(0);
 
+    printf("The return value of previous alarm is %u\n",alarmRet);
+    printf("The return value of previous alarm is %u\n",alarmRet2);
+    
 	write(STDOUT_FILENO, line, n);
 	exit(0);
 }
